e819474e575fbbe9c1e7f7f09053bf66c1d61f08
Moshwen <149946493+Moshwen@users.noreply.github.com>
2024-03-02 19:24:34 +0300

    Update wm5.py

--- Src/wm5.py
+++ Src/wm5.py
@@ -113,18 +113,19 @@ def process_summarize(sIn):
         return "\n".join(str(sentence) for sentence in summarized_text)
     return 'Необходимо заполнить поле стенограмма'
 
+
 with gr.Blocks() as demo:
     gr.Markdown("<span style='font-size: 20px;'> Приложение для Формирования текстов из видео и аудиофайлов </span>")
     with gr.Column(scale=2):
-        t_subtitres_whisper = gr.Radio(["Субтитры", "Распознать аудио"], label = "Вариант исполнения на YouTube:")
-        t_sURL = gr.Text("https://www.youtube.com/watch?v=6EsCI3CbmTk", label = "YouTube - ссылка на страницу с видео")
-        t_subtitres_lang = gr.Text("ru", label = "Язык субтитров (основной либо перевод (указывается код): ru, en ...)")
+        t_subtitres_whisper = gr.Radio(["Субтитры", "Распознать аудио"], label="Вариант исполнения на YouTube:")
+        t_sURL = gr.Text("https://www.youtube.com/watch?v=6EsCI3CbmTk", label="YouTube - ссылка на страницу с видео")
+        t_subtitres_lang = gr.Text("ru", label="Язык субтитров (основной либо перевод (указывается код): ru, en ...)")
         t_video = gr.Video(sources = ['upload'])
-        t_audio = gr.Audio(type = 'filepath', sources = ['upload'])
+        t_audio = gr.Audio(type='filepath', sources=['upload'])
         btn = gr.Button(value="Сформировать стенограмму")
-        t_stenogr = gr.Text("", label = "Стенограмма:")
+        t_stenogr = gr.Text("", label="Стенограмма:")
         btn_summarize = gr.Button(value="Суммаризировать стенограмму")
-        t_summarize = gr.Text("", label = "Суммаризация:")
+        t_summarize = gr.Text("", label="Суммаризация:")
 
         btn.click(process_video, inputs=[t_subtitres_whisper, t_sURL, t_subtitres_lang, t_video, t_audio], outputs=[t_stenogr])
         btn_summarize.click(process_summarize, inputs=[t_stenogr], outputs=[t_summarize])
